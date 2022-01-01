e8dad7bb75e189d6d41041aeed515116d3ebc0b5
Jeffrey Sheehan <jeff.sheehan7>
2022-01-01 16:06:30 +1000

    Comments

--- samples/simple-package/build.py
+++ samples/simple-package/build.py
@@ -12,5 +12,7 @@ default_task = "publish"
 
 @init
 def set_properties(project: Project):
+    # uncomment this to try docker cli instead of
+    # python docker api
     # project.set_property("docker_cli", True)
     pass
